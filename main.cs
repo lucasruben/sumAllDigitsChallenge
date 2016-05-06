7efd9df05c011ceb324c3df416e49114e2fb6cbd
Lucas <lukasrub@gmail.com>
2016-05-06 16:54:11 -0300

    Added Main()

--- main.cs
+++ main.cs
@@ -1,55 +1,64 @@
-public Stack<int> numbersIn(int value)
+using System;
+using System.Collections;
+
+namespace sumAllDigitsNS
 {
-    if (value == 0) return new Stack<int>();
-
-    var numbers = numbersIn(value / 10);
-
-    numbers.Push(value % 10);
-
-    return numbers;
-}
-
-public int auxSum(int N)
-{
-    int result = 0;
-    
-    int[] arrayN = numbersIn(N).ToArray();
-    
-    for(int i = 0; i<arrayN.Length;i++)
+    public Stack<int> numbersIn(int value)
     {
-        result+=arrayN[i];
-        }
+        if (value == 0) return new Stack<int>();
+    
+        var numbers = numbersIn(value / 10);
+    
+        numbers.Push(value % 10);
+    
+        return numbers;
+    }
+    
+    public int auxSum(int N)
+    {
+        int result = 0;
         
-    return result;
-}
-
-public int sumAllDigits(int N)
-{
-    int sum = 0;
-    if(N==1)
-    {
-        sum=1;
-    }
-    else if(N>1)
-    {        
-        for(int i = 1; i <= N; i++)
-        {        
-            if(i.ToString().Length > 1)
-            {
-                sum+=auxSum(i);
+        int[] arrayN = numbersIn(N).ToArray();
+        
+        for(int i = 0; i<arrayN.Length;i++)
+        {
+            result+=arrayN[i];
             }
-            else
-            {
-                sum+=i;
-            }       
-        }
+            
+        return result;
     }
-    else 
+    
+    public int sumAllDigits(int N)
     {
-        Console.WriteLine("N should be greater than 1.");
+        int sum = 0;
+        if(N==1)
+        {
+            sum=1;
         }
+        else if(N>1)
+        {        
+            for(int i = 1; i <= N; i++)
+            {        
+                if(i.ToString().Length > 1)
+                {
+                    sum+=auxSum(i);
+                }
+                else
+                {
+                    sum+=i;
+                }       
+            }
+        }
+        else 
+        {
+            Console.WriteLine("N should be greater than 1.");
+            }      
+                
+        return sum;
+    }
     
-    //Console.WriteLine(sum);
-    
-    return sum;
+    static public void Main ()
+    {
+       Console.WriteLine(sumAllDigits("BMC_TEST_INPUT_MAGIC"));
+    }
 }
