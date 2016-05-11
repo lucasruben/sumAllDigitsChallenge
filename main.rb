9eb0c1f3b34e3df5c79a7b4e2bc6100b3710adab
Lucas <lukasrub@gmail.com>
2016-05-11 15:45:40 -0300

    Added some timeout control
    If it takes more that 3 seconds to solve the problem, it kills the process.

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require 'timeout'
+
 #sumAllDigits in Ruby
 #Sums the digits of a number
 def aux_sum(n)
@@ -8,23 +10,32 @@ def aux_sum(n)
 	end
 	return result
 end
+
 #Given N, sums all the numbers (digits only) including N	
 def sum_all_digits(n)
 	sum = 0
-	if n==1
-		sum = 1
-	elsif n>1
-		for i in 1..n
-			if i.to_s.size > 1
-				sum+=aux_sum(i)
-			else
-				sum+=i
-			end
-		end
-	else
-		puts "N should be greater than 1."
+	time = 3
+	begin
+    	Timeout::timeout(time) do
+        	if n==1
+        		sum = 1
+        	elsif n>1
+        		for i in 1..n
+        			if i.to_s.size > 1
+        				sum+=aux_sum(i)
+        			else
+        				sum+=i
+        			end
+        		end
+        	else
+        		puts "N should be greater than 1."
+        	end
+        end
+	rescue Timeout::Error
+	    puts 'Process not finished in time, killing it'
+	    Process.kill('TERM', Process.pid)
 	end
-    puts sum
+	puts sum
 	return sum
 end
 
